8e69293949fef62e53c09ac1da95f047210754a2
Kamil Khuzzyatov <kamil.khuzzyatov@list.ru>
2024-08-23 10:54:45 +0300

    feat: data and drawing of provinces
    BREAKING CHANGE:
    1) Links to neighboring provinces are added to each province.
    2) Bold-white borders of the kingdoms are drawn.

--- The War Of Kingdoms/script.js
+++ The War Of Kingdoms/script.js
@@ -16,12 +16,28 @@ document.body.append(container);
 
 let skirmishButton = document.querySelector('.skirmishButton');
 
-skirmishButton.addEventListener('click', (event) => {
+skirmishButton.addEventListener('click', (event) => 
+{
     container.remove();
     
-    let xPosition = 50;
-    let yPosition = 50;
+    let xPosition = 100;
+    let yPosition = 1800;
     let provinces = [];
+
+    function provinceObject(x, y, province, provinceType, objectType)
+    {
+        this.x = x;
+        this.y = y;
+        this.province = province;
+        this.provinceType = provinceType;
+        this.objectType = objectType;
+        let bottom = null;
+        let leftBottom = null;
+        let rightBottom = null;
+        let top = null;
+        let leftTop = null;
+        let rightTop = null;
+    }
 
     function generateProvince(position, provinceType, objectType)
     {
@@ -66,7 +82,6 @@ skirmishButton.addEventListener('click', (event) => {
             z-index: 1;
             `;
             document.body.append(province);
-            province = null;
 
             if (objectType != "void")
             {
@@ -83,17 +98,28 @@ skirmishButton.addEventListener('click', (event) => {
                 z-index: 2;
                 `;
                 document.body.append(object);
-                object = null;
             }
+            provinces.push(new provinceObject(xPosition, yPosition, province, provinceType, objectType));
+        }
+        else
+        {
+            provinces.push(new provinceObject(xPosition, yPosition, null, provinceType, objectType));
         }
     }
 
-    for (let i = 1; i <= 11; i++)
+    for (let i = 0; i <= 12; i++)
         {
             switch (i)
             {
+                case 0:
+                    for (let j = 0; j < 17; j++) 
+                    {
+                        generateProvince("top", "void");
+                    }
+                    generateProvince("top-right", "void");
+                    break;
                 case 1:
-                    for (let j = 0; j < 4; j++) 
+                    for (let j = 0; j < 3; j++) 
                     {
                         generateProvince("bottom", "void");
                     }
@@ -304,10 +330,100 @@ skirmishButton.addEventListener('click', (event) => {
                     }
                     generateProvince("bottom", "Free-province", "tree (0)");
                     generateProvince("bottom", "Free-province", "void");
+                    for (let j = 0; j < 3; j++) 
+                    {
+                        generateProvince("bottom", "void");
+                    }
+                    generateProvince("bottom-right", "void");
+                    break;
+                case 12:
+                    for (let j = 0; j < 16; j++) 
+                    {
+                        generateProvince("top", "void");
+                    }
                     break;
             }
         }
     
-    
+    for (i = 0; i < provinces.length; i++)
+    {
+        for (j = 0; j < provinces.length; j++)
+        {
+            if ((provinces[i].y == provinces[j].y - 100) && (provinces[i].x == provinces[j].x))
+            {
+                provinces[i].bottom = provinces[j];
+            }
+            else if ((provinces[i].y == provinces[j].y - 50) && (provinces[i].x == provinces[j].x + 92))
+            {
+                provinces[i].leftBottom = provinces[j];
+            }
+            else if ((provinces[i].y == provinces[j].y - 50) && (provinces[i].x == provinces[j].x - 92))
+            {
+                provinces[i].rightBottom = provinces[j];
+            }
+            else if ((provinces[i].y == provinces[j].y + 100) && (provinces[i].x == provinces[j].x))
+            {
+                provinces[i].top = provinces[j];
+            }
+            else if ((provinces[i].y == provinces[j].y + 50) && (provinces[i].x == provinces[j].x + 92))
+            {
+                provinces[i].leftTop = provinces[j];
+            }
+            else if ((provinces[i].y == provinces[j].y + 50) && (provinces[i].x == provinces[j].x - 92))
+            {
+                provinces[i].rightTop = provinces[j];
+            }
+        }
+    }
+
+    function createBorder(borderName, x, y)
+    {
+        let border = document.createElement('border');
+        border.innerHTML = 
+            `
+                <img src="borders-of-province/border-${borderName}.png">
+            `;
+            border.style =
+            `
+            position: absolute;
+            left: ${x}px;
+            top: ${y}px;
+            z-index: 2;
+            `;
+            document.body.append(border);
+
+    }
+
+    for (i = 0; i < provinces.length; i++)
+    {
+        if ((provinces[i].provinceType != "Free-province") && (provinces[i].provinceType != "void"))
+        {
+            if (provinces[i].provinceType != provinces[i].bottom.provinceType)
+            {
+                createBorder("bottom", provinces[i].x, provinces[i].y);
+                console.log(provinces[i]);
+            }
+            if (provinces[i].provinceType != provinces[i].leftBottom.provinceType)
+            {
+                createBorder("leftBottom", provinces[i].x, provinces[i].y);
+            }
+            if (provinces[i].provinceType != provinces[i].rightBottom.provinceType)
+            {
+                createBorder("rightBottom", provinces[i].x, provinces[i].y);
+            }
+            if (provinces[i].provinceType != provinces[i].top.provinceType)
+            {
+                createBorder("top", provinces[i].x, provinces[i].y);
+                console.log(provinces[i]);
+            }
+            if (provinces[i].provinceType != provinces[i].leftTop.provinceType)
+            {
+                createBorder("leftTop", provinces[i].x, provinces[i].y);
+            }
+            if (provinces[i].provinceType != provinces[i].rightTop.provinceType)
+            {
+                createBorder("rightTop", provinces[i].x, provinces[i].y);
+            }
+        }
+    }
 });
-//<img src="province.png">
