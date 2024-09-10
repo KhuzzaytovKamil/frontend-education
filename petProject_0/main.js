57f3bc956590b974e1cc9c60209e3d17e531fb35
Kamil Khuzzyatov <kamil.khuzzyatov@list.ru>
2024-09-10 22:25:45 +0300

    feat: mined blocks (petProject_0)
    BREAKING CHANGE:
    1) Tracking number of mined blocks of different types is added.
    2) Display of mined blocks is added.

--- petProject_0/main.js
+++ petProject_0/main.js
@@ -24,6 +24,11 @@ let moveDown;
 let moveLeft;
 let moveRight;
 
+let resources = [0, 0, 0, 0, 0, 0, 0, 0];
+let textsOfResources = [];
+let arrayOfBlockTypes = [];
+let lineOfArrayOfBlockTypes = [];
+
 function FrequencyOfHeight(minHeight, maxHeight, frequencyOfBropoutOfType)
 {
     this.minHeight = minHeight;
@@ -31,7 +36,7 @@ function FrequencyOfHeight(minHeight, maxHeight, frequencyOfBropoutOfType)
     this.frequencyOfBropoutOfType = frequencyOfBropoutOfType;
 }
 
-function generateBlock(n0, n1, n2, n3, n4, n5, n6, n7)
+function generateBlock(n0, n1, n2, n3, n4, n5, n6, n7, j)
 {
     let frequencyOfBropoutOfType = [n0, n1, n2, n3, n4, n5, n6, n7];
     let block = document.createElement('block');
@@ -53,6 +58,16 @@ function generateBlock(n0, n1, n2, n3, n4, n5, n6, n7)
         <img src="Blocks/${namesOfBlockSprite[numberOfTypeOfThisBlock]}.png" class="layoutСontroller">
     `;
     document.body.append(block);
+
+    if (j === 0)
+    {
+        arrayOfBlockTypes.push(lineOfArrayOfBlockTypes);
+        lineOfArrayOfBlockTypes = [numberOfTypeOfThisBlock];
+    }
+    else
+    {
+        lineOfArrayOfBlockTypes.push(numberOfTypeOfThisBlock);
+    }
     return block;
 }
 
@@ -91,39 +106,39 @@ function generateGameSpace()
         {
             if (1 <= i && 21 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, -1, 85, 85, 85, 91, 97, 1000));
+                lineOfBlocks.push(generateBlock(-1, -1, 85, 85, 85, 91, 97, 1000, j));
             }
             else if (21 <= i && 36 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, -1, 85, 85, 87, 95, 97, 1000));
+                lineOfBlocks.push(generateBlock(-1, -1, 85, 85, 87, 95, 97, 1000, j));
             }
             else if (36 <= i && 51 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, -1, 85, 85, 90, 97, 1000, 0));
+                lineOfBlocks.push(generateBlock(-1, -1, 85, 85, 90, 97, 1000, 0, j));
             }
             else if (51 <= i && 66 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, -1, 89, 91, 96, 99, 1000, 0));
+                lineOfBlocks.push(generateBlock(-1, -1, 89, 91, 96, 99, 1000, 0, j));
             }
             else if (66 <= i && 81 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, -1, 88, 93, 98, 1000, 0, 0));
+                lineOfBlocks.push(generateBlock(-1, -1, 88, 93, 98, 1000, 0, 0, j));
             }
             else if (81 <= i && 96 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, -1, 90, 94, 1000, 0, 0, 0));
+                lineOfBlocks.push(generateBlock(-1, -1, 90, 94, 1000, 0, 0, 0, j));
             }
             else if (96 <= i && 98 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, 75, 97, 99, 1000, 0, 0, 0));
+                lineOfBlocks.push(generateBlock(-1, 75, 97, 99, 1000, 0, 0, 0, j));
             }
             else if (98 <= i && 100 > i)
             {
-                lineOfBlocks.push(generateBlock(-1, 1000, 0, 0, 0, 0, 0, 0));
+                lineOfBlocks.push(generateBlock(-1, 1000, 0, 0, 0, 0, 0, 0, j));
             }
             else if (100 <= i && 101 > i)
             {
-                lineOfBlocks.push(generateBlock(1000, 0, 0, 0, 0, 0, 0, 0));
+                lineOfBlocks.push(generateBlock(1000, 0, 0, 0, 0, 0, 0, 0, j));
             }
         }
         blocks.push(lineOfBlocks);
@@ -148,10 +163,44 @@ function generateUI()
         <img src="UI/moveButtons/moveLeft.png" class="moveLeft">
         <img src="UI/moveButtons/moveRight.png" class="moveRight">
     </div>
+
+    <div class="blockOfResource">
+        <div class="resource">
+            <img src="Blocks/${namesOfBlockSprite[2]}.png" class="resourceImage">
+            <p id="stone">${resources[0]}</p>
+        </div>
+        <div class="resource">
+            <img src="Blocks/${namesOfBlockSprite[3]}.png" class="resourceImage">
+            <p id="coal">${resources[1]}</p>
+        </div>
+        <div class="resource">
+            <img src="Blocks/${namesOfBlockSprite[4]}.png" class="resourceImage">
+            <p id="iron">${resources[2]}</p>
+        </div>
+        <div class="resource">
+            <img src="Blocks/${namesOfBlockSprite[5]}.png" class="resourceImage">
+            <p id="gold">${resources[3]}</p>
+        </div>
+        <div class="resource">
+            <img src="Blocks/${namesOfBlockSprite[6]}.png" class="resourceImage">
+            <p id="diamond">${resources[4]}</p>
+        </div>
+        <div class="resource">
+            <img src="Blocks/${namesOfBlockSprite[7]}.png" class="resourceImage">
+            <p id="emerald">${resources[5]}</p>
+        </div>
+    </div>
     `;
     UI.classList.add('UI');
     document.body.append(UI);
     
+    textsOfResources.push(document.querySelector('#stone'));
+    textsOfResources.push(document.querySelector('#coal'));
+    textsOfResources.push(document.querySelector('#iron'));
+    textsOfResources.push(document.querySelector('#gold'));
+    textsOfResources.push(document.querySelector('#diamond'));
+    textsOfResources.push(document.querySelector('#emerald'));
+
     moveUp = document.querySelector('.moveUp');
     moveDown = document.querySelector('.moveDown');
     moveLeft = document.querySelector('.moveLeft');
@@ -164,6 +213,11 @@ function generateUI()
             --noobY;
             ++noobVerticalLayer;
             UpdateNoobPosition();
+            if (arrayOfBlockTypes[noobVerticalLayer] ?? false)
+            {
+                resources[arrayOfBlockTypes[noobVerticalLayer][noobHorizontalLayer]]++;
+            }
+            UpdateTextsOfResources();
         }
     });
 
@@ -174,6 +228,11 @@ function generateUI()
             ++noobY;
             --noobVerticalLayer;
             UpdateNoobPosition();
+            if (arrayOfBlockTypes[noobVerticalLayer] ?? false)
+            {
+                resources[arrayOfBlockTypes[noobVerticalLayer][noobHorizontalLayer]]++;
+            }
+            UpdateTextsOfResources();
         }
     });
 
@@ -184,6 +243,11 @@ function generateUI()
             ++noobX;
             ++noobHorizontalLayer;
             UpdateNoobPosition();
+            if (arrayOfBlockTypes[noobVerticalLayer] ?? false)
+            {
+                resources[arrayOfBlockTypes[noobVerticalLayer][noobHorizontalLayer]]++;
+            }
+            UpdateTextsOfResources();
         }
     });
 
@@ -194,8 +258,14 @@ function generateUI()
             --noobX;
             --noobHorizontalLayer;
             UpdateNoobPosition();
+            if (arrayOfBlockTypes[noobVerticalLayer] ?? false)
+            {
+                resources[arrayOfBlockTypes[noobVerticalLayer][noobHorizontalLayer]]++;
+            }
+            UpdateTextsOfResources();
         }
     });
+    UpdateTextsOfResources();
 
     console.log("UI has been created");
     console.log("");
@@ -232,6 +302,14 @@ function UpdateNoobPosition()
         `
             <img src="Ladder.png" class="layoutСontroller">
         `;
+    }
+}
+
+function UpdateTextsOfResources()
+{
+    for (let i = 0; i < 6; i++)
+    {
+        textsOfResources[i].textContent = resources[i + 2];
     }
 }
 
